2d92b9d1dc640c42c03dbdd94d35cfa2f7bf5982
Avayjeet Paul <avaypaul7@gmail.com>
2024-12-21 18:53:12 +0530

    Add files via upload

---
+++ build/precache-manifest.ae356d94937ffcb5043bba93524e36cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7aa9c9ee4ca244aa9a84d092be5f369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c72bbe7d2efe7a21ae9",
+    "url": "/static/css/2.acc1ecba.chunk.css"
+  },
+  {
+    "revision": "a8918fa6fe237c693122",
+    "url": "/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "1c72bbe7d2efe7a21ae9",
+    "url": "/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8918fa6fe237c693122",
+    "url": "/static/js/main.ac55b733.chunk.js"
+  },
+  {
+    "revision": "44d459044b9da8ae1201",
+    "url": "/static/js/runtime-main.6829a894.js"
+  }
+]);
